548ada913f48bdf8562454c2b3b5578b4a8224cb
Dharmendrakumar-Patel <dharmendrakumar.patel@hotmail.com>
2024-06-13 19:47:44 +0530

    setup graphql.

--- money-manger-nestjs/src/app.module.ts
+++ money-manger-nestjs/src/app.module.ts
@@ -5,14 +5,17 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
     imports: [
       ConfigModule.forRoot({isGlobal: true}), 
       GraphQLModule.forRoot<ApolloDriverConfig>({
         driver: ApolloDriver,
+        autoSchemaFile: 'schema.gql'
       }),
-      DatabaseModule
+      DatabaseModule,
+      UserModule
     ],
     controllers: [AppController],
     providers: [AppService],
--- money-manger-nestjs/src/user/user.module.ts
+++ money-manger-nestjs/src/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserResolver } from './graphql/user.reslover';
 
 @Module({
   controllers: [],
-  providers: [UserService]
+  providers: [UserService, UserResolver]
 })
 export class UserModule {}
